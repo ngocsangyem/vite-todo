eef03067526eda3999786a79b7f1a98c34fe3425
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-07-28 16:39:05 +0700

    :lipstick:  add global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import '${pathSrc}/styles/styles.scss';`,
+			},
+		},
+	},
 });
