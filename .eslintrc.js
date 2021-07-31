6a75163b35468fe1cad77bc743eb153570fe7ad8
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-07-31 17:20:24 +0700

    :art: update ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
 	env: {
 		node: true,
 	},
-	extends: ['plugin:vue/vue3-essential', 'prettier'],
+	extends: [
+		'plugin:vue/vue3-essential',
+		'prettier',
+		'@typescript-eslint',
+		'plugin:@typescript-eslint/eslint-recommended',
+	],
 	rules: {
 		// override/add rules settings here, such as:
 		'vue/no-unused-vars': 'error',
@@ -11,4 +16,5 @@ module.exports = {
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'import/prefer-default-export': 0,
 	},
+	parser: '@typescript-eslint/parser',
 };
