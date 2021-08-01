cd9300a28a1104ebc197d48f8d0d4ac47ea2458b
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-08-01 16:43:28 +0700

    :sparkles: todo input

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,21 +1,18 @@
 <template>
 	<div class="todo-header">
-		<textarea-auto-size
-			class="todo-input"
-			:min-height="30"
-			:max-height="100"
-			rows="1"
-		></textarea-auto-size>
+		<todo-input></todo-input>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TextareaAutoSize from './TextareaAutoSize.vue';
+import TodoInput from './TodoInput.vue';
 
 export default defineComponent({
 	components: {
 		'textarea-auto-size': TextareaAutoSize,
+		'todo-input': TodoInput,
 	},
 });
 </script>
@@ -38,14 +35,5 @@ export default defineComponent({
 	padding-left: var(--form-control-padding-x);
 	padding-top: var(--form-control-padding-y);
 	padding-bottom: var(--form-control-padding-y);
-}
-
-.todo-input {
-	width: 100%;
-	vertical-align: middle;
-	line-height: 1;
-	&:focus {
-		outline: none;
-	}
 }
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+	<div contenteditable class="todo-input"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+.todo-input {
+	--height: 30px;
+
+	min-height: var(--height);
+	max-height: 150px;
+	width: 100%;
+
+	text-align: left;
+	line-height: var(--height);
+
+	overflow: auto;
+
+	&:focus {
+		outline: none;
+	}
+}
+</style>
